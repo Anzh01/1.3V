e853ef8bdfb2d0cb6d0893edb5ba64f1bbe30f19
Anzh01 <91677624+Anzh01@users.noreply.github.com>
2022-07-19 08:50:52 +0300

    Add files via upload

--- brands/brands.js
+++ brands/brands.js
@@ -5,35 +5,42 @@ var swiper = new Swiper('.swiper-container', {
     },
     slidesPerView: 'auto',
     direction: 'horizontal',
-    width: 240,
+    spaceBetween: 16,
     centeredSlides: true,
     breakpoints: {
         768: {
             enabled: false,
             onAny() {
                 swiper.destroy(true, true);
-            }
+            },
+            spaceBetween: 0,
         }},
     observer: true,
 });
 
-var brandsBlock = document.querySelector('.swiper-wrapper');
-var button = document.querySelector('.services_button-read-more');
-var buttonText = document.querySelector('.services_button-read-more-text');
-var buttonIcon = document.querySelector('.services_button-icon');
+const brandsBlock = document.querySelector('.swiper-wrapper');
+const button = document.querySelector('.services__button-more');
 
 button.addEventListener('click', function () {
     if (brandsBlock.clientHeight === 180) {
         brandsBlock.classList.add('swiper-wrapper_open');
         brandsBlock.classList.remove('swiper-wrapper_close');
-        buttonText.textContent = 'Скрыть';
-        button.classList.add('services_button-read-more-open')
-        buttonIcon.classList.add('services_button-icon-open');
+        button.textContent = 'Скрыть';
+        button.classList.add('services__button-more_open');
     } else {
         brandsBlock.classList.remove('swiper-wrapper_open');
         brandsBlock.classList.add('swiper-wrapper_close');
-        buttonText.textContent = 'Показать все';
-        button.classList.remove('services_button-read-more-open')
-        buttonIcon.classList.remove('services_button-icon-open');
+        button.textContent = 'Показать все';
+        button.classList.remove('services__button-more_open');
     }
 })
+
+// var windowSize = window.innerWidth;
+// function removeStyle () {
+//     if (windowSize >= 768) {
+//         brandsBlock.style = null;
+//     }
+// }
+// windowSize.addEventListener('resize', function () {
+//     removeStyle()
+// });
